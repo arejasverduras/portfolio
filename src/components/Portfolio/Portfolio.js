66520a1a1258b1afccadd170da1ee62a7aa06c48
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-06-01 21:55:24 +0200

    layout components for portfolio page as comments

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,6 +4,10 @@ export const Portfolio = (props) => {
     return (
         <>
             <h1>Portfolio</h1>
+            {/* <ProjectSlider /> ==> renders ProjectTiles w different styles */}
+            {/* scroll down button (optional) */}
+            {/* <ProjectsFilter />  / <ProjectSearch /> */}
+            {/* <ProjectList /> ==> Renders ProjectTiles */}
         </>
     )
 }
