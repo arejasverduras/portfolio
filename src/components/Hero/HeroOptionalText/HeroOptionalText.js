9b9946345fdb05a87daa24f3ee0c669362664ac0
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-06-01 21:47:39 +0200

    add optionalLogoSection and style

--- src/components/Hero/HeroOptionalText/HeroOptionalText.js
+++ src/components/Hero/HeroOptionalText/HeroOptionalText.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 export const HeroOptionalText = (props) =>{
     return (
+        <>
         <section className='optionalTextSection' id="heroOptionalText">
             <h2>What a legend!</h2>
             <p>And good looking too..</p>
         </section>
+        <section className='optionalLogoSection' id="heroOptionalText">
+                <h2>as trusted by</h2>
+                <ul className='logoList'>
+                    <li>logo1</li>
+                    <li>logo1</li>
+                    <li>logo1</li>
+                </ul>
+        </section>
+        </>
     )
 }
