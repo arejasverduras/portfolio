b4e9500c54355839683c7a6dd0b9e239277855b4
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-06-01 21:30:56 +0200

    add optinional text section as component to Home

---
+++ src/components/Hero/HeroOptionalText/HeroOptionalText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HeroOptionalText = (props) =>{
+    return (
+        <section className='optionalTextSection' id="heroOptionalText">
+            <p>Optional text about something</p>
+        </section>
+    )
+}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Hero } from '../Hero/Hero';
-import { HeroButtons } from '../HeroButtons/HeroButtons';
+import { HeroButtons } from '../Hero/HeroButtons/HeroButtons';
+import { HeroOptionalText } from '../Hero/HeroOptionalText/HeroOptionalText';
 
 export const Home = (props) => {
     return (
         <>
             <Hero />
             <HeroButtons />
+            <HeroOptionalText />
         </>
     )
 }
