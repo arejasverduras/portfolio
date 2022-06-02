c2dbbf4bd1932c36b500e9dcc77533306c80d3ef
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-06-02 13:54:53 +0200

    add top section to About page using the Hero component and additional summary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Hero } from '../Hero/Hero';
 
 export const About = (props) => {
     return (
         <>
-            <h1>About</h1>
+            <section className="aboutTopSection">
+                <Hero />
+                <div className='aboutPageSummary'>
+                    <h1>About</h1>
+                    <p>An athlete by heart, a traveller by day, a poet at night. 
+                        Play music with the tunes of the universe.</p>
+                </div>
+            </section>
         </>
     )
 }
