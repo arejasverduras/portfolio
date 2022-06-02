769d4179a1073aa8f9580a559ec71d73e70e5cb5
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-06-02 15:07:43 +0200

    style about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MichielHero from '../Hero/MichielHero.png';
+import { HeroOptionalText } from '../Hero/HeroOptionalText/HeroOptionalText';
 
 export const About = (props) => {
     return (
@@ -13,6 +14,25 @@ export const About = (props) => {
                         Play music with the tunes of the universe.</p>
                 </div>
             </section>
+            <section className='aboutTextSection'>
+            <h3>Startup Founder turned Developer</h3>
+               <div className='aboutExtraText'>
+                <p>Extremely passionate about building 
+                    beautifull, practical and smart things. 
+                    Self-starting, highly determined, obsessively persistent.
+                </p>
+                <p>Having completed the Front-end section of the 
+                    Full-Stack Career Course on codecademy, 
+                    I am eager to practice my new found skills as a 
+                    Front-end / React Developer.
+                </p>
+                <p> Open for a junior position or Freelance assignment. 
+                    Remote / hybrid preferred. 
+                    <br/>
+                    Get in touch!
+                </p>
+                </div>
+        </section>
         </>
     )
 }
