f1f6801f659eb342c0d69e5f23ea73676e99fff1
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-02 11:36:27 +0200

    remove contact page overlay, enlarge background

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import MichielHero from '../Hero/MichielHero.png';
-import { HeroOptionalText } from '../Hero/HeroOptionalText/HeroOptionalText';
 import { ContactInfo } from './ContactInfo/ContactInfo';
 
 export const Contact = (props) => {
@@ -8,7 +6,6 @@ export const Contact = (props) => {
     return (
         <div className="Contact">
             <section className="contactTopSection">
-                 {/* <img className="contactImage" src='./images/arantxa.png' alt="Michiel's dog Arantxa"></img> */}
                  <ContactInfo className="contactInfoComponent" />
             </section>
             </div>
