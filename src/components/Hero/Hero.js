58586f454d6900ca1e11224822dd2870ad669b76
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-08-30 11:07:46 +0200

    change intro picture

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,13 @@
 import React from "react";
 import MichielHero from './MichielHero.png';
+import MichielPortrait from './MichielPortrait.jpg'
 
 export const Hero = (props) => {
     return (
         <div className="heroSection" >
             <img className="heroImage" src={MichielHero} alt="Michiel with his dog Arantxa on his shoulders"></img>
+            {/* <img className="heroImage" src={MichielPortrait} alt="Michiel on the boulevard of scheveningen, lookin up"></img> */}
+
             <div className="heroTitleSection">
                 <h1 className="heroTitle">Michiel Roukens</h1>
                 <p className='heroSub'>Full-Stack Developer</p>
