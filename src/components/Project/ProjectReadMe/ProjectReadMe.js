ab5c6c67aaecaeedf47e490093f6e16dd7412f16
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-09-02 13:00:55 +0200

    add markdown component

---
+++ src/components/Project/ProjectReadMe/ProjectReadMe.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import Markdown from 'markdown-to-jsx';
+
+export const ProjectReadMe = () => {
+    const file_name = 'README.md';
+    const [post, setPost] = useState('');
+
+    useEffect(()=>{
+        import(`../../../markdown/${file_name}`)
+            .then(res => {
+                fetch(res.default)
+                    .then(res => res.text())
+                    .then(res => setPost(res))
+                    .catch(err => console.log(err));
+            })
+            .catch(err => console.log(err));
+    });
+
+    return (
+        <div className="Readme container">
+            <Markdown>
+                {post}
+            </Markdown>
+        </div>
+    )
+};
