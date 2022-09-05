6d73ff3eb6101fad61b48cc6111a19841fb3bcec
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-09-05 10:28:02 +0200

    remove searchlight on mouseclick

--- src/components/SpotLight/SpotLight.js
+++ src/components/SpotLight/SpotLight.js
@@ -1,4 +1,5 @@
 import {React, useEffect} from 'react';
+import { notInitialized } from 'react-redux/es/utils/useSyncExternalStore';
 
 export const SpotLight = () => {
 
@@ -8,12 +9,26 @@ export const SpotLight = () => {
         spotLightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 148px, #ffd083 150px)`;
     }
 
+    const handleClick = (event) => {
+        const spotLightEl = document.querySelector("#SpotLight");
+        spotLightEl.style.opacity = '0';
+        document.removeEventListener("mousemove", handleMouseMove);
+    }
+
     useEffect(()=>{
         document.addEventListener("mousemove", handleMouseMove);
         return ()=>{
             document.removeEventListener("mousemove", handleMouseMove);
         }
     },)
+
+    useEffect(()=>{
+        document.addEventListener("click", handleClick);
+        
+        return ()=>{
+            document.removeEventListener('click', handleClick)
+        }
+    })
 
     return (
         <div id="SpotLight">
